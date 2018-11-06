845aca5e6556643f45d80b7fac6c9655dc6601da
Hernan Roiz <hernanroiz@gmail.com>
2018-11-06 09:51:52 -0300

    Elimina basura de pruebas anteriores

--- libOld/common/widgets/theme.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-/// El tema para toda la aplicacion
-final appTheme = ThemeData(
-  primaryColor: Colors.blue,
-  accentColor: Colors.red,
-  fontFamily: 'Raleway',
-);
